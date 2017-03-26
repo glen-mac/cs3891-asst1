afa63c27fc81262ab12c85dffd903fffbb8ad815
Luke Cusack <z5078476@unsw.edu.au>
2017-03-26 15:32:57 +1100

    part3 with semaphores

--- kern/asst1/producerconsumer.c
+++ kern/asst1/producerconsumer.c
@@ -1,12 +1,19 @@
 /* This file will contain your solution. Modify it as you wish. */
 #include <types.h>
+#include <lib.h>    /* for kprintf */
 #include "producerconsumer_driver.h"
+#include <synch.h>  /* for P(), V(), sem_* */
 
 /* Declare any variables you need here to keep track of and
    synchronise your bounded. A sample declaration of a buffer is shown
    below. You can change this if you choose another implementation. */
 
 static struct pc_data buffer[BUFFER_SIZE];
+
+static struct semaphore *mutex;
+static struct semaphore *empty;
+static struct semaphore *full;
+
 
 
 /* consumer_receive() is called by a consumer to request more data. It
@@ -15,13 +22,21 @@ static struct pc_data buffer[BUFFER_SIZE];
 
 struct pc_data consumer_receive(void)
 {
-        struct pc_data thedata;
-
-        (void) buffer; /* remove this line when you start */
+        
+        /* remove this line when you start
+         * (void) buffer; 
+         */
 
         /* FIXME: this data should come from your buffer, obviously... */
-        thedata.item1 = 1;
-        thedata.item2 = 2;
+
+        P(full);
+        P(mutex);
+        // kprintf("consumer_receive, empty: %d\n", empty->sem_count);
+        struct pc_data thedata;
+        thedata.item1 = buffer[empty->sem_count].item1;
+        thedata.item2 = buffer[empty->sem_count].item2;
+        V(mutex);
+        V(empty);
 
         return thedata;
 }
@@ -31,7 +46,16 @@ struct pc_data consumer_receive(void)
 
 void producer_send(struct pc_data item)
 {
-        (void) item; /* Remove this when you add your code */
+        P(empty);
+        P(mutex);
+        // kprintf("producer_send, empty: %d\n", empty->sem_count);
+        buffer[empty->sem_count] = item;
+        /* Remove this when you add your code
+         * (void) item; 
+         */
+        V(mutex);
+        V(full);
+
 }
 
 
@@ -42,10 +66,21 @@ void producer_send(struct pc_data item)
 
 void producerconsumer_startup(void)
 {
+        mutex = sem_create("mutex", 1);
+
+        // number of empty slots
+        empty = sem_create("empty", BUFFER_SIZE);
+
+        // number of full slots
+        full = sem_create("full", 0);
 }
 
 /* Perform any clean-up you need here */
 void producerconsumer_shutdown(void)
 {
+        // dun
+        sem_destroy(mutex);
+        sem_destroy(empty);
+        sem_destroy(full);
 }
 
