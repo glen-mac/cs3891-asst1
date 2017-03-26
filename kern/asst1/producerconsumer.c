d7662a053477dafcb7bb047e257c5651eb56286f
Glenn McGuire <glennmcguire9@gmail.com>
2017-03-27 09:36:41 +1100

    Merge Part3 (#4)
    * Part 3 initial solution
    
    * Destroyed allocated memory and checked for allocation failures
    
    * Fixed bug with skipping first data allocation
    
    * Fixed bug with start value of semaphore and added comments
    
    * Final comments

--- kern/asst1/producerconsumer.c
+++ kern/asst1/producerconsumer.c
@@ -1,28 +1,42 @@
 /* This file will contain your solution. Modify it as you wish. */
 #include <types.h>
+#include <synch.h>  /* for P(), V(), sem_* */
+#include <lib.h>    /* for kprintf */
 #include "producerconsumer_driver.h"
 
 /* Declare any variables you need here to keep track of and
    synchronise your bounded. A sample declaration of a buffer is shown
    below. You can change this if you choose another implementation. */
 
-static struct pc_data buffer[BUFFER_SIZE];
-
-
+static struct pc_data buffer[BUFFER_SIZE]; /* Data item buffer */
+int bufStart;   /* Points to the current data item at front of queue */
+int bufEnd;     /* Points to the last element in the queue */
+struct lock *bufLock; /* Lock to control access to buffer and indexes */
+struct semaphore *producer_hold; /* Mutex to control blocking within 
+                                    producer_send() */
+struct semaphore *consumer_hold; /* Mutex to control blocking within 
+                                    consumer_reveive() */
 /* consumer_receive() is called by a consumer to request more data. It
    should block on a sync primitive if no data is available in your
    buffer. */
 
 struct pc_data consumer_receive(void)
 {
+        /* Block if 10 consumers have emptied the buffer without a producer 
+         * creating a signal. Effectively the number of items in buf. */
+        P(consumer_hold);
+
         struct pc_data thedata;
+ 
+        /* critical region, acquire lock */
+        lock_acquire(bufLock);
+        thedata = buffer[bufStart];
+        bufStart = (bufStart+1) % BUFFER_SIZE;
+        lock_release(bufLock);
+        /* critical region, release lock */
 
-        (void) buffer; /* remove this line when you start */
-
-        /* FIXME: this data should come from your buffer, obviously... */
-        thedata.item1 = 1;
-        thedata.item2 = 2;
-
+        /* Signal to the producer that a consumer has consumed an item */
+        V(producer_hold);
         return thedata;
 }
 
@@ -31,7 +45,20 @@ struct pc_data consumer_receive(void)
 
 void producer_send(struct pc_data item)
 {
-        (void) item; /* Remove this when you add your code */
+        /* Block if 10 producers have filled the buffer without a consumer 
+         * creating a signal. Effectively the number of free spots in buf. */
+        P(producer_hold);
+
+        /* critical region, acquire lock */
+        lock_acquire(bufLock);
+        bufEnd = (bufEnd + 1) % BUFFER_SIZE;
+        buffer[bufEnd] = item;
+        lock_release(bufLock);
+        /* critical region, release lock */
+
+        /* Signal to the consumer that a producer has produced an item */
+        V(consumer_hold);
+        return;
 }
 
 
@@ -42,10 +69,27 @@ void producer_send(struct pc_data item)
 
 void producerconsumer_startup(void)
 {
+        bufStart = 0;
+        bufEnd = BUFFER_SIZE - 1;
+
+        /* create locks and semaphores and make 
+         * sure they allocated correctly */
+        
+        producer_hold = sem_create("producer_hold", BUFFER_SIZE);
+        KASSERT(producer_hold != 0);
+        consumer_hold = sem_create("consumer_hold", 0);
+        KASSERT(consumer_hold != 0);
+        
+        bufLock = lock_create("bufLock");
+        KASSERT(bufLock != 0);
 }
 
 /* Perform any clean-up you need here */
 void producerconsumer_shutdown(void)
 {
+        /* clean up all memory */
+        lock_destroy(bufLock);
+        sem_destroy(producer_hold);
+        sem_destroy(consumer_hold);
 }
 
